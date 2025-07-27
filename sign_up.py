e883325670e254fc3aff20b1443d12ed1dddb5fe
Kaan Buke <kabuke13@gmail.com>
2025-07-27 21:22:54 +0200

    add traveler class

--- sign_up.py
+++ sign_up.py
@@ -1,8 +1,16 @@
 import re
 
 class User:
-    def __init__(self, email, ac_name, ac_type):
+    # Define attributes that fit all users
+    def __init__(self, email, ac_type):
         self.email = email 
-        self.ac_name = ac_name 
         self.ac_type = ac_type
+
+class Traveler(User):
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age 
+
+
 
